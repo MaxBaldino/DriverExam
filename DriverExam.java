dd279536a93ae5d005830cd86135d13ae52c1ad6
MaxBaldino <43002640+MaxBaldino@users.noreply.github.com>
2019-03-06 08:37:47 -0500

    Add files via upload

--- DriverExam.java
+++ DriverExam.java
@@ -1,4 +1,28 @@
 
 public class DriverExam {
 
+	private char[] correctAnswers = new char[20];
+	
+	public double totalCorrect(char[] studentAnswers) {
+		int correct = 0;
+		for (int i = 0; correctAnswers.length < i; i++) {
+			if (studentAnswers[i] == correctAnswers[i]) {
+				correct++;
+			}
+		}
+		return correct/correctAnswers.length;
+	}
+	
+	public boolean passed(char[] studentAnswers) {
+		if (totalCorrect(studentAnswers) >= 0.75) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static void Main(String[] args) {
+		
+	}
 }
